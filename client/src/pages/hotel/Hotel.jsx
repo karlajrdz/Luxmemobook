6af064a6489c7d002f1caa733e59cee9211b3136
Tu Nombre <karlajrdz@gmail.com>
2022-07-10 11:45:45 +0200

    Starting the second part

--- client/src/pages/hotel/Hotel.jsx
+++ client/src/pages/hotel/Hotel.jsx
@@ -12,10 +12,12 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 
-const Hotel = () => {
+const Hotel=()=>{
+  //const location=useLocation()
+  //console.log(location)
   const [slideNumber, setSlideNumber] = useState(0);
   const [open, setOpen] = useState(false);
-
+//const{data,loading,error}=useFetch('/hotels/')
   const photos = [
     {
       src: "https://cf.bstatic.com/xdata/images/hotel/max1280x900/261707778.jpg?k=56ba0babbcbbfeb3d3e911728831dcbc390ed2cb16c51d88159f82bf751d04c6&o=&hp=1",
